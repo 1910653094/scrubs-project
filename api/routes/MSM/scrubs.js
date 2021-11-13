bf0309bc9d37afc2f4275c631ab63a0d6b0302b0
JoachimRnd <renardjoachim@gmail.com>
2021-11-13 14:58:35 +0000

    Add - 'Get Personal Currently Borrowed Scrubs

--- api/routes/MSM/scrubs.js
+++ api/routes/MSM/scrubs.js
@@ -4,6 +4,7 @@ const db = require('../../helper/elephantSQL');
 
 const router = express.Router();
 
+// Personal overdue scrubs : GET /msm/scrubs/overdue/
 router.get('/overdue', async (req, res, next) => {
     const id_employee = req.query.id;
 
@@ -20,20 +21,21 @@ router.get('/overdue', async (req, res, next) => {
     }
 });
 
-/*
-  newUser.save().then(() => {
-    jwt.sign({ email: newUser.email}, jwtSecret,{ expiresIn: LIFETIME_JWT }, (err, token) => {
-      if (err) {
-        console.error("POST users/ :", err);
-        return res.status(500).send(err.message);
-      }
-      console.log("POST users/ token:", token);
-      return res.json({ email: newUser.email, token });
-    });
-  });
+// Personal currently borrowed scrubs : GET /msm/scrubs/borrowed/currently/
+router.get('/borrowed/currently', async (req, res, next) => {
+    const id_employee = req.query.id;
 
+    try {
+        let result = await db.query(
+            'SELECT sc.id_scrub, sc.borrowed, sc.borrowed_date, sc.return_date, sc.id_scrub_type, sc.id_employee ' + 
+            'FROM scrub sc '+
+            'WHERE sc.borrowed = \'true\' AND sc.return_date IS NULL AND sc.id_employee = $1;', [id_employee]);
 
+        return res.status(200).json(result.rows);
+    } catch (err) {
+        return res.status(400).json({ error: err });
+    }
+});
 
-*/
 
 module.exports = router;
