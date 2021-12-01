090476b7d212a39cbbfc04789ce2d7827e37ba56
David Bester <43348086+davidbes@users.noreply.github.com>
2021-12-01 22:13:25 +0000

    add logic to profile page (#22)
    * add logic to profile page
    
    * add key to detailed information component
    
    * remove unnescessary files
    
    * add imports for hsstaffdetails pages
    
    * add fixes to features
    
    Co-authored-by: David Bešter <davidbester@Davids-MacBook-Pro-15.local>

--- client/src/redux/store/store.js
+++ client/src/redux/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../features/authSlice/authSlice';
+import profileReducer from '../features/profileSlice/profileSlice';
 
 export const store = configureStore({
 	reducer: {
 		auth: authReducer,
+		profile: profileReducer,
 	},
 });
