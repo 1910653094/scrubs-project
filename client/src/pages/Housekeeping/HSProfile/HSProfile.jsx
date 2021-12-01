1e186e7e62a6f56c333fd092a1b96b60e76240b3
David Bešter <davidbester@Davids-MacBook-Pro-15.local>
2021-12-01 20:20:04 +0000

    add logic to profile page

--- client/src/pages/Housekeeping/HSProfile/HSProfile.jsx
+++ client/src/pages/Housekeeping/HSProfile/HSProfile.jsx
@@ -1,19 +1,40 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { PageWrapper, Card } from '../../../layouts';
-import { DetailedInformation } from '../../../components';
+import { DetailedInformation, Spinner } from '../../../components';
+import {
+	cleanProfileData,
+	getProfile,
+} from '../../../redux/features/profileSlice/profileSlice';
 
 const HSProfile = () => {
+	const dispatch = useDispatch();
+
+	const { data, isLoading, error } = useSelector(({ profile }) => profile);
+	useEffect(() => {
+		dispatch(getProfile());
+		return () => {
+			dispatch(cleanProfileData());
+		};
+	}, []);
+
 	return (
 		<PageWrapper>
 			<h2>Profile</h2>
 			<Card>
-				<DetailedInformation
-					items={[
-						{ attr: 'Full Name', val: 'dr. Mike Wazowski' },
-						{ attr: 'Email', val: 'mike.wazowski@gmail.com' },
-						{ attr: 'Profession', val: 'Doctor' },
-						{ attr: 'Gender', val: 'Male' },
-					]}
-				/>
+				{isLoading ? (
+					<Spinner />
+				) : (
+					<DetailedInformation
+						title={data.name}
+						items={[
+							{ attr: 'Email', val: data.email },
+							{ attr: 'Gender', val: data.gender },
+							{ attr: 'Professiion', val: 'Housekeeping' },
+							{ attr: 'Hospital', val: data.hospital },
+						]}
+					/>
+				)}
 			</Card>
 		</PageWrapper>
 	);
