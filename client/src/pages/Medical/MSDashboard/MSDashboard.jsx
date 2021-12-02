9bcb9719ac325a610b8dc15a873e90cbae70914a
David Bešter <davidbester@Davids-MacBook-Pro-15.local>
2021-12-02 17:32:31 +0000

    fix MSDashboard

--- client/src/pages/Medical/MSDashboard/MSDashboard.jsx
+++ client/src/pages/Medical/MSDashboard/MSDashboard.jsx
@@ -1,70 +1,73 @@
-import React from "react";
-import RadioButtons from "../../../components/DashboardComponents/RadioButtons";
-import { CustomTable, DetailsLink } from "../../../components";
-import MSPageWrapper from "../../../layouts/MSPageWrapper/MSPageWrapper";
-import "./MSDashboard.scss";
-import { ReactComponent as ArrowRight } from "../../../assets/icons/Arrow-Right.svg";
-import ActionLink from "../../../components/ActionLink/ActionLink";
+import React, { useState } from 'react';
+import RadioButtons from '../../../components/DashboardComponents/RadioButtons';
+import { CustomTable, DetailsLink } from '../../../components';
+import MSPageWrapper from '../../../layouts/MSPageWrapper/MSPageWrapper';
+import './MSDashboard.scss';
+import { ReactComponent as ArrowRight } from '../../../assets/icons/Arrow-Right.svg';
+import ActionLink from '../../../components/ActionLink/ActionLink';
 const MSDashboard = () => {
-  const headers = [
-    {
-      id: "type",
-      label: "Type",
-      minWidth: 150,
-    },
-    {
-      id: "items",
-      label: "Items",
-      minWidth: 100,
-    },
-    {
-      id: "action",
-      minWidth: 100,
-      align: "right",
-    },
-  ];
-  const data = [
-    {
-      type: "Gloves",
-      items: 10,
-      action: <ActionLink path="/m/dashboard" arrow={<ArrowRight />} />,
-    },
-    {
-      type: "Gloves",
-      items: 10,
-      action: <ActionLink path="/m/dashboard" arrow={<ArrowRight />} />,
-    },
-    {
-      type: "Gloves",
-      items: 10,
-      action: <ActionLink path="/m/dashboard" arrow={<ArrowRight />} />,
-    },
-    {
-      type: "Gloves",
-      items: 10,
-      action: <ActionLink path="/m/dashboard" arrow={<ArrowRight />} />,
-    },
-  ];
+	const [viewOption, setViewOption] = useState('borrowed');
+	const headers = [
+		{
+			id: 'type',
+			label: 'Type',
+			minWidth: 150,
+		},
+		{
+			id: 'items',
+			label: 'Items',
+			minWidth: 100,
+		},
+		{
+			id: 'action',
+			minWidth: 50,
+			align: 'right',
+		},
+	];
+	const data = [
+		{
+			type: 'Gloves',
+			items: 10,
+			action: <ActionLink path='/m/dashboard' arrow={<ArrowRight />} />,
+		},
+		{
+			type: 'Gloves',
+			items: 10,
+			action: <ActionLink path='/m/dashboard' arrow={<ArrowRight />} />,
+		},
+		{
+			type: 'Gloves',
+			items: 10,
+			action: <ActionLink path='/m/dashboard' arrow={<ArrowRight />} />,
+		},
+		{
+			type: 'Gloves',
+			items: 10,
+			action: <ActionLink path='/m/dashboard' arrow={<ArrowRight />} />,
+		},
+	];
 
-  return (
-    <MSPageWrapper>
-      <div className="ms-your-borrowings">
-        <div className="container">
-          <div className="title">Your Borrowings</div>
-          <div className="banner">
-            <RadioButtons />
-          </div>
-          <div className="ms-table">
-            {data.length >= 1 ? (
-              <CustomTable columns={headers} rows={data} />
-            ) : (
-              <div>No data available</div>
-            )}
-          </div>
-        </div>
-      </div>
-    </MSPageWrapper>
-  );
+	console.log(viewOption);
+	return (
+		<MSPageWrapper>
+			<div className='ms-your-borrowings'>
+				<div className='container'>
+					<div className='title'>Your Borrowings</div>
+					<div className='banner'>
+						<RadioButtons returnValue={setViewOption} />
+					</div>
+
+					<div className='ms-table'>
+						{data.length >= 1 ? (
+							<CustomTable columns={headers} rows={data} />
+						) : (
+							<div>No data available</div>
+						)}
+					</div>
+				</div>
+			</div>
+		</MSPageWrapper>
+	);
 };
 
 export default MSDashboard;
